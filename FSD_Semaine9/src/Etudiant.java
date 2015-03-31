1472a433c4bfff0849a65b8f8f34834650ca9d74
NiGHT-RiDER <NiGHTRiDERgithub@tfwno.gf>
2015-03-31 17:41:31 +0200

    modification sur etudiant

--- FSD_Semaine9/src/Etudiant.java
+++ FSD_Semaine9/src/Etudiant.java
@@ -9,6 +9,31 @@ public class Etudiant {
 
 	public String getNom() {
 		return nom;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Etudiant other = (Etudiant) obj;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		return true;
 	} 
 	
 	
