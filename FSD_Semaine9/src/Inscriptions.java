a296799430020623bb495c4fb80652c3c69078b3
NiGHT-RiDER <NiGHTRiDERgithub@tfwno.gf>
2015-03-31 22:23:43 +0200

    suite de l'implementation des niveaux NON ACHEVE

--- FSD_Semaine9/src/Inscriptions.java
+++ FSD_Semaine9/src/Inscriptions.java
@@ -2,120 +2,94 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-
 public class Inscriptions {
-
-	private HashSet<Etudiant> etudiantInscrits;
-	private HashSet<Etudiant> etudiantsEnAttente;
-	private LinkedList<Etudiant> fileDAttente; 
+	
+	private static final int nbNiveaux = Niveau.values().length;
 	private int nbMaxInscrits;
+
+	private HashSet<Etudiant>[] etudiantsInscrits;
+	private HashSet<Etudiant>[] etudiantsEnAttente;
+	private LinkedList<Etudiant>[] fileDAttente; 
 
 	// nombreMax est le maximum d'inscrits autorise dans le groupe de conversation
 	public Inscriptions(int nombreMax){
-		this.etudiantInscrits = new HashSet<Etudiant>(nombreMax);
-		this.etudiantsEnAttente = new HashSet<Etudiant>();
-		this.fileDAttente = new LinkedList<Etudiant>();
-		this.nbMaxInscrits = nombreMax;
+		this.etudiantsInscrits = new HashSet[nbNiveaux];
+		this.etudiantsEnAttente= new HashSet[nbNiveaux];
+		this.fileDAttente = new LinkedList[nbNiveaux];
+		for (int indice = 0 ; indice < Niveau.values().length ; indice++) {
+			etudiantsEnAttente[indice] = new HashSet<Etudiant>(nombreMax);
+			etudiantsInscrits[indice] = new HashSet<Etudiant>(nombreMax);
+			fileDAttente[indice] = new LinkedList<Etudiant>();
+		}
+		
 	}
 
 	// renvoie le nombre d'inscrits, c-a-d le nombre d'etudiant faisant partie du groupe de conversation
-	public int nombreIscrits(){
-		return etudiantInscrits.size();
+	public int nombreIscrits(int niveau){
+		return etudiantsInscrits[niveau].size();
 	}
 	// verifie si l'etudiant fait partie du groupe de conversation
 	public boolean estInscrit(Etudiant etudiant){
-		return etudiantInscrits.contains(etudiant);
+		int indiceNiveau = etudiant.getNiveau().getIndice();
+		return etudiantsInscrits[indiceNiveau].contains(etudiant);
 	}
 
 	// verifie si l'etudiant se trouve dans la liste d'attente
 	public boolean estEnAttente(Etudiant etudiant){
 		// A COMPLETER
-		return etudiantsEnAttente.contains(etudiant);
 	}
 
 	// renvoie false si plus de place
 	// il ne faut pas verifier si l'etudiant est deja present
 	public boolean inscrire(Etudiant etudiant){ 
 
-		// il reste de la place pour une inscription
-		if ( placeRestanteInscrits() ) {
-			return etudiantInscrits.add(etudiant);
-		}
-		return false;
 	}
 
 	// renvoie false si l'etudiant n'est pas inscrit
 	// il faut juste supprimer l'etudiant
 	// cette methode ne s'occupe pas d'attribuer une place a un etudiant en attente
 	public boolean desinscrire(Etudiant etudiant){
-		return etudiantInscrits.remove(etudiant);
+
 	}
 
 
 	// il ne faut pas verifier si l'etudiant est deja present
 	public void mettreEnAttente(Etudiant etudiant){
-		if (etudiantInscrits.contains(etudiant)) {
-			return ; 
-		}
-		if (etudiantsEnAttente.add(etudiant)) {
-			fileDAttente.addLast(etudiant);
-		}
 
+		
 	}
 
 	// renvoie null s’il n’y a pas d’etudiant en attente
 	public Etudiant attribuerPlace(){ 
 		// A COMPLETER
-		if (fileDAttente.isEmpty()) {
-			return null;
-		}
-		Etudiant toRemove = fileDAttente.removeFirst();
-		if (!estEnAttente(toRemove)) {
-			return null;
-		}
-
-		if (placeRestanteInscrits()) {
-			retirerFileAttente(toRemove);
-			inscrire(toRemove);
-			return toRemove;
-		} else if (!placeRestanteInscrits()) {
-			mettreEnAttente(toRemove);
-		}
 		return null;
 	}
 
 	private boolean placeRestanteInscrits() {
-		return etudiantInscrits.size() < nbMaxInscrits;
+
 	}
 
 
 	// renvoie false si l'etudiant n'est pas en attente
 	public boolean retirerFileAttente(Etudiant etudiant){
-		return etudiantsEnAttente.remove(etudiant);
+
 	}
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		String toReturn = "--- Gens Inscrits ---";
-		for (Iterator<Etudiant> iterator = etudiantInscrits.iterator(); iterator
-				.hasNext();) {
-			Etudiant etudiant = (Etudiant) iterator.next();
-			toReturn += "\n" + etudiant.toString();
-		}
-
-		toReturn += "\n--- Gens en attente ---";
-		for (Iterator<Etudiant> iterator = etudiantsEnAttente.iterator(); iterator
-				.hasNext();) {
-			Etudiant etudiant = (Etudiant) iterator.next();
-			if (etudiantsEnAttente.contains(etudiant)) {
-				toReturn += "\n" + etudiant.toString();
-			}
-
-		}
-		return toReturn;
 	}
-
+	
+	public enum Niveau { 
+		DEBUTANT(0), INTERMEDIAIRE(1) , EXPERT(2);
+		private int indice ;
+		private Niveau(int indice){
+			this.indice = indice ; 
+		}
+		public int getIndice(){
+			return this.indice;
+		}
+	};
 
 
 }
